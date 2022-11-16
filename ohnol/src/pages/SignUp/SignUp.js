ca06f69bf8e9aa7f92d9bed20aa1359d6a8d88a1
bookhabit <qnrgoqlt@naver.com>
2022-11-17 03:38:28 +0900

    회원가입 오류처리

--- ohnol/src/pages/SignUp/SignUp.js
+++ ohnol/src/pages/SignUp/SignUp.js
@@ -7,13 +7,16 @@ import "../../pages/SignUp/SignUp.css";
 
 const SignUp = () => {
   const navigate = useNavigate();
+  const [errors, setErrors] = useState("");
+  const [pwdErrors, setPwdErrors] = useState("");
   // 회원가입
   const [registerInputs, setRegisterInputs] = useState({
     email: "",
     username: "",
     pwd: "",
+    confirmPwd: "",
   });
-  const { email, username, pwd } = registerInputs;
+  const { email, username, pwd, confirmPwd } = registerInputs;
 
   // 회원가입 onchange 이벤트
   const onChangeRegister = (e) => {
@@ -25,25 +28,36 @@ const SignUp = () => {
   };
 
   // 회원가입 기능
-  const register = (e) => {
-    // Request API.
-    e.preventDefault();
-    axios
-      .post("http://13.125.105.33:8080/auth/signup", {
-        username: username,
-        email: email,
-        password: pwd,
-      })
-      .then((response) => {
-        // Handle success.
-        console.log(response);
 
-        navigate("/loginForm");
-      })
-      .catch((error) => {
-        // Handle error.
-        console.log("An error occurred:", error.response);
-      });
+  const register = (e) => {
+    e.preventDefault();
+    // 비밀번호 , 비밀번호 확인 에러처리
+    if (pwd === confirmPwd) {
+      axios
+        .post("http://13.125.105.33:8080/auth/signup", {
+          username: username,
+          email: email,
+          password: pwd,
+        })
+        .then((response) => {
+          // Handle success.
+          console.log(response);
+
+          navigate("/loginForm");
+        })
+        .catch((err) => {
+          // Handle error.
+          console.log("An error occurred:", err.response);
+          if (err.response.code === "DUPLICATE_USER") {
+            setErrors("이미 존재하는 사용자입니다.");
+          } else if (err.response?.status === 400) {
+            setErrors("이메일 또는 패스워드를 확인해주세요");
+            console.log(err);
+          }
+        });
+    } else {
+      setPwdErrors("비밀번호가 일치하지 않습니다.");
+    }
   };
 
   return (
@@ -72,6 +86,9 @@ const SignUp = () => {
           onChange={onChangeRegister}
           name="email"
         />
+        {errors ? (
+          <p className="errorMessage"> 이미 존재하는 사용자입니다.</p>
+        ) : null}
         <input
           type="password"
           className="input-field"
@@ -80,7 +97,15 @@ const SignUp = () => {
           onChange={onChangeRegister}
           name="pwd"
         />
-
+        <input
+          type="password"
+          className="input-field"
+          placeholder="Confirm Password"
+          required
+          onChange={onChangeRegister}
+          name="confirmPwd"
+        />
+        {pwdErrors ? <p className="errorMessage">{pwdErrors}</p> : null}
         <button className="submit" onClick={register}>
           <p>회원가입하기</p>
         </button>
