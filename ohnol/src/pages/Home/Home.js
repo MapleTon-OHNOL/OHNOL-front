facc116811864fd6fb6853846482c14f1badd83b
bookhabit <qnrgoqlt@naver.com>
2022-11-16 22:31:41 +0900

    헤더와 로그인창 UI 구현

--- ohnol/src/pages/Home/Home.js
+++ ohnol/src/pages/Home/Home.js
@@ -9,32 +9,9 @@ const Home = () => {
   // 로그인상태
   const [isLoggedIn, setIsLoggedIn] = useRecoilState(LoginState);
 
-  // 로그아웃기능
-  const logoutHandler = (e) => {
-    e.preventDefault();
-    const accesToken = localStorage.getItem("user");
-    axios
-      .post(
-        "http://52.78.126.242:8080/auth/logout",
-        (axios.defaults.headers.common[
-          "Authorization"
-        ] = `Bearer ${accesToken}`)
-      )
-      .then((response) => {
-        console.log(response);
-      })
-      .catch((error) => {
-        console.log(error.response);
-      });
-    localStorage.removeItem("user"); // 로컬스토리지에 있는 토큰삭제
-    setIsLoggedIn(false); // 로그인전역상태 false변경
-  };
-  console.log(isLoggedIn);
   return (
     <section id="main" className="my-5 py-5 px-4 mx-auto">
       <div className="introduce">오랜만에 놀러와!</div>
-      {/* 로그아웃 */}
-      <button onClick={logoutHandler}>로그아웃</button>
     </section>
   );
 };
--- ohnol/src/pages/Login/Login.js
+++ ohnol/src/pages/Login/Login.js
@@ -1,14 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from "../../imgs/logo/logo.png";
+import "./Login.css";
 
 const Login = () => {
   return (
-    <div className="mainLogin">
-      <div className="nav">
-        <Link to="/loginForm">로그인</Link>
-        <Link to="/signUp">회원가입</Link>
+    <div className="mainLoginContainer">
+      <div className="mainLogin">
+        <img src={logo} alt="로고이미지" />
+        <p className="bigContent">
+          <span>오랜만에</span>
+          <span>놀러와~~!</span>
+        </p>
+        <p className="content">
+          망설이지마 NO NO NO~! <br />
+          평소에 하고 싶었던 말, 기회가 없어서 못한 말<br />
+          <span>O</span> <span>NOL</span> 을 통해 전달해보세요!
+          <br />
+          크리스마스 이브가 되면 당신의 메세지가 전달됩니다.
+        </p>
       </div>
-      <div>이미지창</div>
     </div>
   );
 };
